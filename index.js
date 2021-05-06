b5af7bef3d3424d50a16c1a688b2a94fe651a4be
v-briese <valentine.briese.07@gmail.com>
2021-05-06 16:36:54 -0700

    Seperate context item

--- index.js
+++ index.js
@@ -29,39 +29,44 @@ export default class AddonInstaller extends Plugin {
 
 				if (isPlugin || channelId == "753291485100769411")
 					res.props.children.push(
-						<ContextMenu.Item
-							label={`Install ${isPlugin ? "Plugin" : "Theme"}`}
-							id="addon-installer"
-							action={() => {
-								try {
-									require("child_process").execSync(
-										`git clone ${args[0].message.embeds[0].fields[0].rawValue}`,
-										{
-											stdio: [0, 1, 2], // TBH I have absolutely no idea what this is for
-											cwd: require("path").resolve(
-												__dirname,
-												isPlugin ? "../" : "../../themes"
-											),
-										}
-									);
+						<>
+							<ContextMenu.Separator />
+							<ContextMenu.Group>
+								<ContextMenu.Item
+									label={`Install ${isPlugin ? "Plugin" : "Theme"}`}
+									id="addon-installer"
+									action={() => {
+										try {
+											require("child_process").execSync(
+												`git clone ${args[0].message.embeds[0].fields[0].rawValue}`,
+												{
+													stdio: [0, 1, 2], // TBH I have absolutely no idea what this is for
+													cwd: require("path").resolve(
+														__dirname,
+														isPlugin ? "../" : "../../themes"
+													),
+												}
+											);
 
-									vizality.api.notifications.sendToast({
-										header: (
-											<ToastHeader isSuccess={true} isPlugin={isPlugin} />
-										),
-										timeout: 2000,
-									});
-								} catch (error) {
-									this.log("Addon installation failed!\n", error);
-									vizality.api.notifications.sendToast({
-										header: (
-											<ToastHeader isSuccess={false} isPlugin={isPlugin} />
-										),
-										timeout: 3000,
-									});
-								}
-							}}
-						/>
+											vizality.api.notifications.sendToast({
+												header: (
+													<ToastHeader isSuccess={true} isPlugin={isPlugin} />
+												),
+												timeout: 2000,
+											});
+										} catch (error) {
+											this.log("Addon installation failed!\n", error);
+											vizality.api.notifications.sendToast({
+												header: (
+													<ToastHeader isSuccess={false} isPlugin={isPlugin} />
+												),
+												timeout: 3000,
+											});
+										}
+									}}
+								/>
+							</ContextMenu.Group>
+						</>
 					);
 
 				return res;
