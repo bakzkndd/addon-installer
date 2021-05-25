70d5d703826cba061870e70ac9ccd9c6172cca5b
Yahya Wessam <yahyawessam2002@gmail.com>
2021-05-26 02:51:00 +0300

    Solved undefined href problem Now enitre addonURL is undefined (:

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ export default class AddonInstaller extends Plugin {
         if (channelID === '755005584322854972') {
           if (vizality.manager.plugins.isInstalled('00pccompat') && vizality.manager.plugins.isEnabled('00pccompat')) {
             // eslint-disable-next-line no-useless-escape
-            const addonURL = args[0].href.match(/(http|https)\:\/\/github\.com\/(.+)\/(.*?)\.git/);
+            const githubRegex = /(http|https)\:\/\/github\.com\/(.+)\/(.*?)\.git/;
+            const addonURL = args[0].href?.match(githubRegex);
             const addonID = addonURL.split('/').pop().toLowerCase();
             const isPlugin = channelID === '755005584322854972' && true;
             const addonIsInstalled = vizality.manager[
