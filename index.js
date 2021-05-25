7ae5abcb03f7d4069c9bfae78addb5bd5295c4c6
Yahya Wessam <yahyawessam2002@gmail.com>
2021-05-26 01:22:17 +0300

    esinit formating on top

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { patch, unpatchAll } from '@vizality/patcher';
 import { getModule } from '@vizality/webpack';
 import { ContextMenu } from '@vizality/components';
 
-export default class AddonInstaller extends Plugin { 
+export default class AddonInstaller extends Plugin {
   start () {
     patch(
       'addon-installer',
@@ -13,80 +13,79 @@ export default class AddonInstaller extends Plugin {
       'default',
       (args, res) => {
         const channelID = args[0].channel?.id;
-		if (channelID == '755005584322854972') {
-			if (vizality.manager.plugins.isInstalled('00pccompat') && vizality.manager.plugins.isEnabled('00pccompat')) {
-				const url = args[0].message.content[0].find(e => e);
-				const addonURL = url.rawValue.startswith('<') ? url.rawValue.replace(/<|>/g, '') : url.rawValue;
-				const addonID = addonURL.split('/').pop().toLowerCase();
-				const isPlugin = channelID == '755005584322854972' && true;
-				const addonIsInstalled = vizality.manager[
-					isPlugin == 'plugins'
-				].keys.includes(addonID);
+        if (channelID === '755005584322854972') {
+          if (vizality.manager.plugins.isInstalled('00pccompat') && vizality.manager.plugins.isEnabled('00pccompat')) {
+            const url = args[0].message.content[0].find(e => e);
+            const addonURL = url.rawValue.startswith('<') ? url.rawValue.replace(/<|>/g, '') : url.rawValue;
+            const addonID = addonURL.split('/').pop().toLowerCase();
+            const isPlugin = channelID === '755005584322854972' && true;
+            const addonIsInstalled = vizality.manager[
+              isPlugin === 'plugins'
+            ].keys.includes(addonID);
 
-				if (isPlugin)
-				res.props.children.push(
-					<>
-					<ContextMenu.Separator />
-					<ContextMenu.Group>
-						<ContextMenu.Item
-						label={`${addonIsInstalled ? 'Uninstall' : 'Install'} ${
-							isPlugin == 'Plugin'
-						}`}
-						id="addon-installer"
-						action={async () => {
-							if (addonIsInstalled) {
-								await vizality.manager['plugin'].uninstall(
-									addonID
-								);
-							} else {
-								await vizality.manager['plugin'].install(addonURL);
-							}
-						}}
-						/>
-					</ContextMenu.Group>
-					</>
-				)
-			}
-		} else {
-        const isPlugin = channelID === '753291447523868753' && true;
+            if (isPlugin) {
+              res.props.children.push(
+                <>
+                  <ContextMenu.Separator />
+                  <ContextMenu.Group>
+                    <ContextMenu.Item
+                      label={`${addonIsInstalled ? 'Uninstall' : 'Install'} ${isPlugin === 'Plugin'
+                      }`}
+                      id="addon-installer"
+                      action={async () => {
+                        if (addonIsInstalled) {
+                          await vizality.manager.plugin.uninstall(
+                            addonID
+                          );
+                        } else {
+                          await vizality.manager.plugin.install(addonURL);
+                        }
+                      }}
+                    />
+                  </ContextMenu.Group>
+                </>
+              );
+            }
+          }
+        } else {
+          const isPlugin = channelID === '753291447523868753' && true;
 
-        if (isPlugin || channelID === '753291485100769411') {
-          const addonURL = args[0].message?.embeds[0]?.fields[0].rawValue;
-          const addonID = addonURL.split('/').pop().toLowerCase();
+          if (isPlugin || channelID === '753291485100769411') {
+            const addonURL = args[0].message?.embeds[0]?.fields[0].rawValue;
+            const addonID = addonURL.split('/').pop().toLowerCase();
 
-          const pluginsOrThemes = isPlugin ? 'plugins' : 'themes';
+            const pluginsOrThemes = isPlugin ? 'plugins' : 'themes';
 
-          const addonIsInstalled = vizality.manager[
-            pluginsOrThemes
-          ].keys.includes(addonID);
+            const addonIsInstalled = vizality.manager[
+              pluginsOrThemes
+            ].keys.includes(addonID);
 
-          res.props.children.push(
-            <>
-              <ContextMenu.Separator />
-              <ContextMenu.Group>
-                <ContextMenu.Item
-                  label={`${addonIsInstalled ? 'Uninstall' : 'Install'} ${
-                    isPlugin ? 'Plugin' : 'Theme'
-                  }`}
-                  id="addon-installer"
-                  action={async () => {
-                    if (addonIsInstalled) {
-                      await vizality.manager[pluginsOrThemes].uninstall(
-                        addonID
-                      );
-                    } else {
-                      await vizality.manager[pluginsOrThemes].install(addonURL);
-                    }
-                  }}
-                />
-              </ContextMenu.Group>
-            </>
-          );
+            res.props.children.push(
+              <>
+                <ContextMenu.Separator />
+                <ContextMenu.Group>
+                  <ContextMenu.Item
+                    label={`${addonIsInstalled ? 'Uninstall' : 'Install'} ${isPlugin ? 'Plugin' : 'Theme'
+                    }`}
+                    id="addon-installer"
+                    action={async () => {
+                      if (addonIsInstalled) {
+                        await vizality.manager[pluginsOrThemes].uninstall(
+                          addonID
+                        );
+                      } else {
+                        await vizality.manager[pluginsOrThemes].install(addonURL);
+                      }
+                    }}
+                  />
+                </ContextMenu.Group>
+              </>
+            );
+          }
+
+          return res;
         }
-
-        return res;
-      }
-	  });
+      });
   }
 
   stop () {
