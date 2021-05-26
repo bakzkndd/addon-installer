4f1b7eb22b6c6933af4d221f82d970bc5a645c9d
Yahya Wessam <yahyawessam2002@gmail.com>
2021-05-27 01:37:21 +0300

    Do requested changes

--- index.js
+++ index.js
@@ -16,35 +16,31 @@ export default class AddonInstaller extends Plugin {
       (args, res) => {
         const channelID = args[0].channel?.id;
         if (channelID === '755005584322854972') {
-          if (vizality.manager.plugins.isInstalled('00pccompat') && vizality.manager.plugins.isEnabled('00pccompat')) {
+          if (vizality.manager.plugins.isEnabled('00pccompat')) {
             const addonURL = args[0].message?.content.match(githubRegex)[0];
             const addonID = addonURL.split('/').pop().toLowerCase();
-            const isPlugin = channelID === '755005584322854972' && true;
             const addonIsInstalled = vizality.manager.plugins.keys.includes(addonID);
-
-            if (isPlugin) {
-              res.props.children.push(
-                <>
-                  <ContextMenu.Separator />
-                  <ContextMenu.Group>
-                    <ContextMenu.Item
-                      label={`${addonIsInstalled ? 'Uninstall' : 'Install'} ${'Plugin'
-                      }`}
-                      id="addon-installer"
-                      action={async () => {
-                        if (addonIsInstalled) {
-                          await vizality.manager.plugins.uninstall(
-                            addonID
-                          );
-                        } else {
-                          await vizality.manager.plugins.install(addonURL);
-                        }
-                      }}
-                    />
-                  </ContextMenu.Group>
-                </>
-              );
-            }
+            res.props.children.push(
+              <>
+                <ContextMenu.Separator />
+                <ContextMenu.Group>
+                  <ContextMenu.Item
+                    label={`${addonIsInstalled ? 'Uninstall' : 'Install'} ${'Plugin'
+                    }`}
+                    id="addon-installer"
+                    action={async () => {
+                      if (addonIsInstalled) {
+                        await vizality.manager.plugins.uninstall(
+                          addonID
+                        );
+                      } else {
+                        await vizality.manager.plugins.install(addonURL);
+                      }
+                    }}
+                  />
+                </ContextMenu.Group>
+              </>
+            );
           }
         } else {
           const isPlugin = channelID === '753291447523868753' && true;
